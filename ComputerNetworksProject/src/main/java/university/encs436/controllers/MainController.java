38a3c8b615892af4eeae23a18c6a59421742b38d
saleemhamo <saleemhamo@saleems-mbp>
2020-11-07 18:59:58 +0200

    code with comments!

--- ComputerNetworksProject/src/main/java/university/encs436/controllers/MainController.java
+++ ComputerNetworksProject/src/main/java/university/encs436/controllers/MainController.java
@@ -1,5 +1,7 @@
 package university.encs436.controllers;
 
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +16,7 @@ import university.encs436.services.MainService;
 @Controller
 public class MainController {
 
-	
+	/** Main controllers that deals with http requests and calls service functions! **/
 	
 
 	@Autowired 
@@ -22,31 +24,55 @@ public class MainController {
 	
 	
 	@RequestMapping("/")
-	public String index(Model model, HttpServletRequest request) {
+	public String index(Model model, HttpServletRequest request) { // this method is fired whenever 'index.html' is called
+		
+		printRequest(request);
 	    return service.index();
 	}
 	
 	@RequestMapping("/partOne")
-	public String partOne(HttpServletRequest request) {
+	public String partOne(HttpServletRequest request) { // this method is fired whenever 'partOne.html' is called
+		
+		printRequest(request);
 		return service.partOne();
 	}
 	
 	@RequestMapping("/1")
-	public ModelAndView firstRequest(HttpServletRequest request) {
+	public ModelAndView firstRequest(HttpServletRequest request) { // this method is fired whenever '/1' is called
+		
+		printRequest(request);
 		return service.firstRequest(request.getRemoteAddr(), request.getRemotePort());
 	}
 	
 	
 	@RequestMapping("/2")
-	public String secondRequest(HttpServletRequest request) {
+	public String secondRequest(HttpServletRequest request) { // this method is fired whenever '/2' is called
+		
+		printRequest(request);
 		return service.secondRequest();
 	}
 	
 	
 	@RequestMapping("/3")
-	public String thirdRequest(HttpServletRequest request) {
+	public String thirdRequest(HttpServletRequest request) { // this method is fired whenever '/3' is called
+		
+		printRequest(request);
 		return service.thirdRequest();
 	}
+	
+	
+	public void printRequest(HttpServletRequest request) { // This method prints out the received HTTP request
+		System.out.println("---------------------------------------------");
+		System.out.println(request.getMethod() + "/" + request.getProtocol());
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while(headerNames.hasMoreElements()) {
+		  String headerName = headerNames.nextElement();
+		  System.out.println("" + headerName + ":" + request.getHeader(headerName));
+		}
+		System.out.println("---------------------------------------------");
+
+	}
+	
 	
 }
 
--- ComputerNetworksProject/src/main/java/university/encs436/services/MainService.java
+++ ComputerNetworksProject/src/main/java/university/encs436/services/MainService.java
@@ -9,18 +9,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Service
 public class MainService {
 
-	public String index() {
+	public String index() { // return 'index.html' page
 		return "index";
 	}
 
 	
-	public String partOne() {
+	public String partOne() {  // return 'partOne.html' page
 		
 		return "partOne";
 		
 	}
 	
-	public ModelAndView firstRequest(String clientIP, int serverPort) {
+	public ModelAndView firstRequest(String clientIP, int serverPort) { // return 'firstPage.html' page after sending query parameters to it.
 
 		Map<String, Object> params = new HashMap<>();
 		params.put("clientIP", clientIP);
@@ -31,13 +31,13 @@ public class MainService {
 
 	}
 
-	public String secondRequest() {
+	public String secondRequest() {// return 'secondPage.html' page
 
 		return "secondPage";
 
 	}
 
-	public String thirdRequest() {
+	public String thirdRequest() { // return 'thirsPage.html' page
 
 		return "thirdPage";
 
